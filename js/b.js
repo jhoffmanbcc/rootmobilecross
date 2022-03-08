1520bdc415d16b20c60e206f2135e63db46bebbf
Just Hoff <jhoffmanbcc@gmail.com>
2022-03-08 08:41:37 -0800

    update with variables

--- js/b.js
+++ js/b.js
@@ -6,3 +6,17 @@
 
 console.log('hey there.. we meet again ;) ');
 alert('wait.. and do not forget to celebrate');
+
+
+/* Now we will create a variable */
+/* Follow along with W3 schools on variables.. because W3 schools is amazing 
+https://www.w3schools.com/js/js_variables.asp.  */
+
+
+// we will create a couple variables 
+
+
+var instructor = 60;
+var rating = 40;
+var score = instructor + rating;
+console.log(score);
